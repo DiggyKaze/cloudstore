ee64bab5c46867ad9bd1f133bab51dad0357b9cf
DiggyKaze <paerzae@gmail.com>
2026-06-22 12:21:30 +0200

    Trying to set Stage Profile as active

--- app/src/test/java/com/caniz/AppTest.java
+++ app/src/test/java/com/caniz/AppTest.java
@@ -3,7 +3,9 @@ package com.caniz;
 //import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("stage")
 @SpringBootTest
  class AppTests{
   //  @Disabled("Test is not ready yet")
