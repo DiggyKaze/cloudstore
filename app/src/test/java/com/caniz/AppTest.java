974b8c357c338e70d78964aa2f3403a21c34d146
DiggyKaze <paerzae@gmail.com>
2026-06-22 11:59:42 +0200

    Testing AppTest changes

--- app/src/test/java/com/caniz/AppTest.java
+++ app/src/test/java/com/caniz/AppTest.java
@@ -1,10 +1,12 @@
 package com.caniz;
 
-import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
  class AppTests{
-    @Disabled("Test is not ready yet")
+  //  @Disabled("Test is not ready yet")
     @Test
 	public void contextLoads() throws Exception{}
 }
