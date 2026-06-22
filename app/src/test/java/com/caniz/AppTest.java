a2cfdb2a174cead775b258c1005b8c70a6f0c7d0
DiggyKaze <paerzae@gmail.com>
2026-06-22 13:02:27 +0200

    Change back app test

--- app/src/test/java/com/caniz/AppTest.java
+++ app/src/test/java/com/caniz/AppTest.java
@@ -1,14 +1,14 @@
 package com.caniz;
 
-//import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("stage")
-@SpringBootTest
+//@ActiveProfiles("stage")
+//@SpringBootTest
  class AppTests{
-  //  @Disabled("Test is not ready yet")
+    @Disabled("Test is not ready yet")
     @Test
 	public void contextLoads() throws Exception{}
 }
